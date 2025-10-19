ca33e1bd0a08db32fb89a49e436a6bf624e43e0d
Dori <sky980221@naver.com>
2025-10-19 11:55:44 +0900

    feat(ui): implement Console input and output connection

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        //System.out.println("유저 입력 : " + input);
+        int result = Calculate.add(input);
+        //System.out.println("계산 결과 : " + result);
     }
 }
